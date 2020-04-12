7b353ad760b9d2098fd8f2954c08e43c470a6bb2
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2020-04-12 18:35:47 +0800

    init commit

---
+++ src/main/java/art/cctcc/game/demo/Main.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Jonathan Chang, Chun-yien <ccy@musicapoetica.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package art.cctcc.game.demo;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.system.AppSettings;
+import com.jme3.scene.shape.Box;
+
+/**
+ *
+ * @author Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+
+        Main app = new Main();
+
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle("My Awesome Game");
+        app.setSettings(settings);
+
+        app.start();
+
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+}
